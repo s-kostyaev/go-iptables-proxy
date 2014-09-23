85836c371d8233b13ecf6de4caeaf4aa9429d84a
Kostyaev Sergey <s.kostyaev@office.ngs.ru>
2014-09-23 17:42:22 +0000

    fix lines lengh

--- proxy.go
+++ proxy.go
@@ -18,7 +18,8 @@ type Node struct {
 	Port int
 }
 
-func NewProxy(sourceIP string, sourcePort int, destIP string, destPort int) *Proxy {
+func NewProxy(sourceIP string, sourcePort int,
+	destIP string, destPort int) *Proxy {
 	var proxy Proxy
 	proxy.Source.IP = sourceIP
 	proxy.Source.Port = sourcePort
@@ -31,9 +32,11 @@ func (proxy *Proxy) Enable() error {
 	if proxy.Enabled {
 		return nil
 	}
-	cmd := exec.Command("iptables", "-t", "nat", "-A", "PREROUTING", "-p", "tcp",
-		"-d", proxy.Source.IP, "--dport", fmt.Sprint(proxy.Source.Port),
-		"-j", "DNAT", "--to", fmt.Sprintf("%s:%d", proxy.Dest.IP, proxy.Dest.Port))
+	cmd := exec.Command("iptables", "-t", "nat", "-A", "PREROUTING",
+		"-p", "tcp", "-d", proxy.Source.IP,
+		"--dport", fmt.Sprint(proxy.Source.Port),
+		"-j", "DNAT", "--to",
+		fmt.Sprintf("%s:%d", proxy.Dest.IP, proxy.Dest.Port))
 	cmd.Stdin = strings.NewReader("")
 	var out bytes.Buffer
 	cmd.Stdout = &out
@@ -42,7 +45,8 @@ func (proxy *Proxy) Enable() error {
 		return err
 	}
 	cmd = exec.Command("iptables", "-A", "FORWARD", "-d", proxy.Dest.IP,
-		"-p", "tcp", "--dport", fmt.Sprint(proxy.Dest.Port), "-j", "ACCEPT")
+		"-p", "tcp", "--dport",
+		fmt.Sprint(proxy.Dest.Port), "-j", "ACCEPT")
 	cmd.Stdin = strings.NewReader("")
 	cmd.Stdout = &out
 	err = cmd.Run()
@@ -57,9 +61,11 @@ func (proxy *Proxy) Disable() error {
 	if !proxy.Enabled {
 		return nil
 	}
-	cmd := exec.Command("iptables", "-t", "nat", "-D", "PREROUTING", "-p", "tcp",
+	cmd := exec.Command("iptables", "-t", "nat", "-D",
+		"PREROUTING", "-p", "tcp",
 		"-d", proxy.Source.IP, "--dport", fmt.Sprint(proxy.Source.Port),
-		"-j", "DNAT", "--to", fmt.Sprintf("%s:%d", proxy.Dest.IP, proxy.Dest.Port))
+		"-j", "DNAT", "--to",
+		fmt.Sprintf("%s:%d", proxy.Dest.IP, proxy.Dest.Port))
 	cmd.Stdin = strings.NewReader("")
 	var out bytes.Buffer
 	cmd.Stdout = &out
